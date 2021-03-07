d82d757fdd2347c3b7f8e58f6bcced749db91094
Jorge Jimenez <jorgejimenez@Jorges-MacBook-Air-2.local>
2021-03-07 18:10:59 -0500

    better

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 // Body-parser is the Node.js body parsing middleware. It is responsible for parsing the incoming request bodies in a middleware before you handle it.
 // to check body-parser https://www.npmjs.com/package/body-parser
 const bodyParser = require("body-parser");
+
 // middleware same as morgan
 const pino = require("express-pino-logger")();
 const client = require("twilio")(
@@ -9,10 +10,14 @@ const client = require("twilio")(
   process.env.TWILIO_AUTH_TOKEN
 );
 
+const cors = require("cors");
+
+
 const server = express();
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 server.use(pino);
+server.use(cors());
 
 server.post("/api/messages", (req, res) => {
   res.header("Content-Type", "application/json");
