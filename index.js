9a6e60427dbd517b320761adf2c1662ae66a212e
Jorge Jimenez <jorgejimenez@Jorges-MacBook-Air-2.local>
2021-03-07 17:35:19 -0500

    better

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 
 const server = require('./server.js');
 
-const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+
+server.listen(process.env.PORT || 3000);
